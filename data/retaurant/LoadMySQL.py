331ade9ff9913bb8521d6bb01876d800c98fff58
SangwookWoo <wjddm3@naver.com>
2023-06-06 20:36:11 +0900

    #12 Path 변수 방식 변경
    절대경로 -> os.path.join 활용

--- data/retaurant/LoadMySQL.py
+++ data/retaurant/LoadMySQL.py
@@ -3,6 +3,8 @@ import sqlalchemy
 from sqlalchemy import create_engine
 import pymysql
 
+filePath, fileName = os.path.split(__file__)
+
 def uploadToMysql(df, table_name):
     # MySQL 데이터베이스 연결 정보 설정
     host = 'host'  # 호스트 주소
@@ -21,17 +23,16 @@ def uploadToMysql(df, table_name):
     df.to_sql(name=table_name, con=engine, if_exists='append', index=False)
 
 # 맛집정보
-df = pd.read_csv('C:/last_project/MangoPlate/data/지하철역별맛집정보최종_20230531.csv')
-table_name = 'restaurant'
+df = pd.read_csv(os.path.join(filePath, 'data', '지하철역별맛집정보최종_20230531.csv'))
 uploadToMysql(df, table_name)
 
 # 운영 시간
-run_time = pd.read_csv('C:/last_project/MangoPlate/data/지하철역별맛집운영시간_20230531.csv')
+run_time = pd.read_csv(os.path.join(filePath, 'data', '지하철역별맛집운영시간_20230531.csv'))
 table_name = 'restaurant_runtime'
 uploadToMysql(run_time, table_name)
 
 # 휴무 시간
-break_time = pd.read_csv('C:/last_project/MangoPlate/data/지하철역별맛집휴식시간_20230531.csv')
+break_time = pd.read_csv(os.path.join(filePath, 'data', '지하철역별맛집휴식시간_20230531.csv'))
 table_name = 'restaurant_breaktime'
 uploadToMysql(break_time, table_name)
 
