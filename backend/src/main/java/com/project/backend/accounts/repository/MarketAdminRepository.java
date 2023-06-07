50fb210fb51238e24dba66ad8974642cf97ddb74
ByeongChang <als6068@naver.com>
2023-06-07 10:42:09 +0900

    #14-modifyTable gitflow-feature-stash:
    #14 - entity에 @Data에 들어있는 @ToString 어노테이션을 삭제해주고,
    repository에 repository임을 명시하는 @Repository 어노테이션 추가
    repository와 entity 디렉토리도 형태가 분리가 안되어 있어서 분리함

--- backend/src/main/java/com/project/backend/accounts/repository/MarketAdminRepository.java
+++ backend/src/main/java/com/project/backend/accounts/repository/MarketAdminRepository.java
@@ -1,7 +1,9 @@
 package com.project.backend.accounts.repository;
 
-import com.project.backend.accounts.repository.entity.MarketAdmin;
+import com.project.backend.accounts.entity.MarketAdmin;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MarketAdminRepository extends JpaRepository<MarketAdmin, String> {
 }
