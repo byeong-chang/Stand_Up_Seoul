0ea7389c66076ea0378f216102b83d6208005d51
Minsoo0405 <munsi0405@naver.com>
2023-06-11 22:51:19 +0900

    #30 - 로그인 및 회원가입 기능 구현하기
    Restaurant 엔터티 수정으로 인해 레포지토리도 수정
    
    * market_admin 에서 restaurant_admin으로 클래스명 변경
    * id(기본 키), restaurant_id 컬럼 추가

--- backend/src/main/java/com/project/backend/accounts/repository/RestaurantAdminRepository.java
+++ backend/src/main/java/com/project/backend/accounts/repository/RestaurantAdminRepository.java
@@ -2,5 +2,5 @@ package com.project.backend.accounts.repository;
 
 import com.project.backend.accounts.entity.MarketAdmin;
 import org.springframework.data.jpa.repository.JpaRepository;
-public interface MarketAdminRepository extends JpaRepository<MarketAdmin, String> {
+public interface RestaurantAdminRepository extends JpaRepository<MarketAdmin, String> {
 }
