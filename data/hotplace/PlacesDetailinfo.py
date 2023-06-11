1b7a8a405207716736d963b265a2499a3f31a79a
SangwookWoo <wjddm3@naver.com>
2023-06-11 22:14:38 +0900

    #17 api 활용 명소별 상세 정보 추출
    - json으로 두 개 파일 뽑아놨습니다.

--- data/hotplace/PlacesDetailinfo.py
+++ data/hotplace/PlacesDetailinfo.py
@@ -1,5 +1,10 @@
 import requests
 import json
+from plugins import db_connector
+from plugins import seoupl00api
+import pandas as pd
+import json
+
 
 def detail_search(contentId, contentTypeId):
     url = 'http://apis.data.go.kr/B551011/KorService1/detailIntro1'
@@ -9,21 +14,41 @@ def detail_search(contentId, contentTypeId):
         '_type' : 'json',
         'contentId' : contentId, 
         'contentTypeId' : contentTypeId,
-        'serviceKey' : 'servicekey'}
+        'serviceKey' : seoupl00api.apikey()}
     data = requests.get(url, params = params)
-    return data.json()
+    # return data.text
+    return data.json()['response']['body']['items']['item'][0]
 
-def return_items(contentId, contentTypeId):
-    result = detail_search(contentId, contentTypeId)
-    result = result['response']['body']['items']['item'][0]
+def call_df():
+    # 0 : id, 1 : content_id , 2 : content_type_id
+    conn, cursor = db_connector.mysql()
+    cursor.execute('select id, content_id, content_type_id from hotplaces;')
+    df = pd.DataFrame(cursor.fetchall())
+    return df
 
-    usefee = result.get('usefee')  # 값이 없으면 None으로 설정
-    infocenterculture = result.get('infocenterculture')
-    usetimeculture = result.get('usetimeculture')
-    restdateculture = result.get('restdateculture')
-    parkingculture = result.get('parkingculture')
-    chkbabycarriageculture = result.get('chkbabycarriageculture')
-    chkpetculture = result.get('chkpetculture')
-    chkcreditcardculture = result.get('chkcreditcardculture')
+def main():
+    df = call_df()
+    df = df.tail(700)
 
-    return usefee, infocenterculture, usetimeculture, restdateculture, parkingculture, chkbabycarriageculture, chkpetculture, chkcreditcardculture
+    info_list = []  # 빈 리스트 생성
+
+    for idx in df.index:
+        try:
+            id = int(df.loc[idx, 0])
+            content_id = int(df.loc[idx, 1])
+            content_type_id = int(df.loc[idx, 2])
+            info = detail_search(content_id, content_type_id)
+            info['id'] = id
+            
+            info_list.append(info)  # JSON 데이터를 리스트에 추가
+        except Exception as e:
+            print(e)
+            pass
+        
+    # JSON 데이터를 파일로 저장
+    # (api 호출이 하루당 1000개 제한)
+    with open('./data/hotplaces_detail.json', 'w') as file:
+        json.dump(info_list, file)
+
+if __name__ == '__main__':
+    main()     
