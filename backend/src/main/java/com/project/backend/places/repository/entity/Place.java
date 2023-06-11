8cfc8b1c8fe67b9c5445f4bcbeb14576f58fe494
ByeongChang <als6068@naver.com>
2023-06-11 16:44:08 +0900

    Place 테이블에 위경도 및 이미지 값이 추가되어 entity와 dto를 수정하였습니다.

--- backend/src/main/java/com/project/backend/places/repository/entity/Place.java
+++ backend/src/main/java/com/project/backend/places/repository/entity/Place.java
@@ -33,4 +33,14 @@ public class Place {
     // 연결 테이블(MEMBER_PRODUCT)쪽이 외래키를 갖고있기 때문에, 연결 테이블이 연관관계의 주인이다.
     @OneToMany(mappedBy = "place")
     private List<PlaceSubway> placeSubways;
+
+    @Column(name = "place_image",length = 255)
+    private String placeImage;
+
+    @Column(name = "mapx")
+    private double mapx;
+
+    @Column(name = "mapy")
+    private double mapy;
+
 }
