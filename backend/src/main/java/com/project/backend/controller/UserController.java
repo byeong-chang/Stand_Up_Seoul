8b4c5967eb9c75894a36b22290b96929047d6931
Minsoo0405 <munsi0405@naver.com>
2023-06-16 15:50:55 +0900

    #30-login and signup
    * UserController : 사용자 관련 기능 및 회원가입과 로그인 기능을 구현
    * BackendController : 사용하지 않는 어노테이션  삭제

--- backend/src/main/java/com/project/backend/controller/UserController.java
+++ backend/src/main/java/com/project/backend/controller/UserController.java
@@ -1,10 +1,62 @@
 package com.project.backend.controller;
 
+import com.project.backend.accounts.form.UserCreateForm;
+import com.project.backend.accounts.repository.entity.Users;
+import com.project.backend.accounts.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
-@RequiredArgsConstructor
+import javax.validation.Valid;
+import java.time.LocalDateTime;
+
 @Controller
+@RequestMapping("/user")
+//@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/signup")
+    public String signup(UserService userService) {
+        return "signup_form";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login_form";
+    }
+
+    @PostMapping("/signup")
+    public String signup(@Valid UserCreateForm userCreateForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "signup_form";
+        }
+
+        if (!userCreateForm.getPassword1().equals(userCreateForm.getPassword2())) {
+            bindingResult.rejectValue("password2", "passwordInCorrect", "2개의 패스워드가 일치하지 않습니다.");
+            return "signup_form";
+        }
+
+        try {
+            userService.create(userCreateForm.getEmail(), userCreateForm.getPassword1(), LocalDateTime.parse(userCreateForm.getBirth()), userCreateForm.getPhoneNumber(), userCreateForm.getNickname());
+        } catch (DataIntegrityViolationException e) {
+            e.printStackTrace();
+            bindingResult.rejectValue("signupFailed", "이미 등록된 사용자입니다.");
+            return "signup_form";
+        } catch (Exception e) {
+            e.printStackTrace();
+            bindingResult.rejectValue("signupFailed", e.getMessage());
+            return "signup_form";
+        }
+        return "redirect:/";
+    }
 
 }
