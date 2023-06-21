bb1a86d463f0fdca719bc979b5d39a554ed14e83
ByeongChang <als6068@naver.com>
2023-06-21 19:59:49 +0900

    #88 jwt code
    user 엔티티, dto, repository, service, controller를 수정해주고,
    security 디렉토리에 JwtAuthenticationFilter, TokenProvider, WebMvcConfig, WebSecurityConfig를 추가해줌

--- backend/src/main/java/com/project/backend/accounts/repository/UsersRepository.java
+++ backend/src/main/java/com/project/backend/accounts/repository/UsersRepository.java
@@ -7,5 +7,7 @@ public interface UsersRepository extends JpaRepository<Users, String> {
 
     Users findByEmail(String email);
     Boolean existsByEmail(String email);
+    Boolean existsByNickname(String nickname);
+    Boolean existsByPhoneNumber(String phoneNumber);
     Users findByEmailAndPassword(String email, String password);
 }
