6c667e4bf61cdbd0a993bacef07dd6a9cf68d0ba
ByeongChang <als6068@naver.com>
2023-06-25 14:54:54 +0900

    #100-hotplace like
    레스토랑 보드에 like 여부를 추가해주었습니다.
    api 전송시 boardLike 값이 -1 이라면 좋아요를 누르지 않은 상태이고, 양수형 -hotplace board에 대한 좋아요 처리입니다. 반환값이 -1이라면 해당 유저가 해당 hotplace에 좋아요를 누르지 않은 것이고, 양수형 int 값으로 들어온다면, board_like 테이블의 id 값을 반환한 것이기에 좋아요를 누른 상태입니다.

--- backend/src/main/java/com/project/backend/accounts/repository/HotplaceLikeRepository.java
+++ backend/src/main/java/com/project/backend/accounts/repository/HotplaceLikeRepository.java
@@ -2,6 +2,11 @@ package com.project.backend.accounts.repository;
 
 import com.project.backend.accounts.repository.entity.HotplaceLike;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface HotplaceLikeRepository extends JpaRepository<HotplaceLike,Integer> {
+
+    @Query(value = "select * from hotplace_like where hotplace_id = :hotplaceId and users_id = :userId",nativeQuery = true)
+    HotplaceLike findByUserIdAndHotplaceId(@Param("hotplaceId") int hotplaceId, @Param("userId") int userId);
 }
--- backend/src/main/java/com/project/backend/general/returnType/HotplaceType.java
+++ backend/src/main/java/com/project/backend/general/returnType/HotplaceType.java
@@ -1,5 +1,6 @@
 package com.project.backend.general.returnType;
 
+import com.project.backend.accounts.dto.HotplaceLikeDto;
 import com.project.backend.accounts.dto.HotplaceReviewDto;
 import com.project.backend.places.dto.HotplacesDto;
 import lombok.Data;
@@ -9,5 +10,6 @@ import java.util.List;
 @Data
 public class HotplaceType {
     HotplacesDto hotplacesDto;
+    HotplaceLikeDto hotplaceLikeDto;
     List<HotplaceReviewDto> hotplaceReviewDtos;
 }
