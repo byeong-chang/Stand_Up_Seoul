a78fc2ed4eb515eb77f4478efcda1667e83770da
Minsoo0405 <munsi0405@naver.com>
2023-06-27 01:25:16 +0900

    #95-User_register
    마이페이지에서 사용자 정보 수정에 대한 컨트롤러와 서비스 수정

--- backend/src/main/java/com/project/backend/controller/MyPageController.java
+++ backend/src/main/java/com/project/backend/controller/MyPageController.java
@@ -1,56 +1,78 @@
-//package com.project.backend.controller;
-//
-//import com.project.backend.accounts.dto.UsersDto;
-//import com.project.backend.accounts.service.MyPageService;
-//import com.project.backend.accounts.validator.NicknameFormValidator;
-//import lombok.RequiredArgsConstructor;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.security.core.annotation.AuthenticationPrincipal;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import java.util.List;
-//
-//@Slf4j
-//@RestController
-//@RequiredArgsConstructor
-//@RequestMapping("/user")
-//public class MyPageController {
-//
-//    private final MyPageService myPageService;
-//    private final NicknameFormValidator nicknameFormValidator;
-//
-//    @GetMapping("mypage")
-//    public ResponseEntity<List<UsersDto>> retrieve(@AuthenticationPrincipal String userId) {
-//        List<UsersDto> entity = myPageService.retrieve(userId);
-//        return ResponseEntity.ok(entity);
-//    }
+package com.project.backend.controller;
 
-    /*// Nickname
-    @InitBinder("nicknameForm")
-    public void nicknameFormInitBinder(WebDataBinder webDataBinder) {
-        webDataBinder.addValidators(nicknameFormValidator);
-    }*/
+import com.project.backend.accounts.dto.UsersDto;
+import com.project.backend.accounts.repository.UsersRepository;
+import com.project.backend.accounts.repository.entity.Users;
+import com.project.backend.accounts.service.MyPageService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
 
-    /*@GetMapping("mypage")
-    public String nicknameForm(Users user, Model model) {
-        model.addAttribute(user);
-        model.addAttribute(new NicknameForm(user.getNickname()));
-        return "/user";
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/user")
+public class MyPageController {
+
+    private final MyPageService myPageService;
+    private final UsersRepository usersRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @GetMapping("/mypage")
+    public ResponseEntity<UsersDto> retrieve(@AuthenticationPrincipal String userId) {
+        UsersDto entity = myPageService.retrieve(userId);
+        return ResponseEntity.ok(entity);
     }
 
-    @PostMapping("mypage")
-    public String updateNickname(Users user, @Valid NicknameForm nicknameForm, Errors errors, Model model, RedirectAttributes attributes) {
-        if (errors.hasErrors()) {
-            model.addAttribute(user);
-            return "/user";
+    @PostMapping("/mypage")
+    public ResponseEntity<UsersDto> updateUser(@AuthenticationPrincipal String userId, @RequestBody UsersDto usersDto, Errors errors) {
+        Users user = usersRepository.findById(Integer.parseInt(userId));
+
+        if (user != null) {
+            if (errors.hasErrors()) {
+                UsersDto entity = myPageService.retrieve(userId);
+                return ResponseEntity.badRequest().body(entity);
+            }
+
+            // 업데이트할 필드 값을 설정
+            if (usersDto.getNickname() != null) {
+                user.setNickname(usersDto.getNickname());
+            }
+            if (usersDto.getPassword() != null) {
+                user.setPassword(passwordEncoder.encode(usersDto.getPassword()));
+            }
+            if (usersDto.getUserAddress() != null) {
+                user.setUserAddress(usersDto.getUserAddress());
+            }
+            if (usersDto.getPhoneNumber() != null) {
+                user.setPhoneNumber(usersDto.getPhoneNumber());
+            }
+
+            usersRepository.save(user);
+            myPageService.login(user);
+
+            UsersDto updatedUsersDto = myPageService.convertToUsersDto(user);
+            return ResponseEntity.ok(updatedUsersDto);
+
         }
-        userService.updateNickname(user, nicknameForm.getNickname());
-        attributes.addFlashAttribute("message", "닉네임을 수정하였습니다.");
-        return "redirect:" + "/user";
-    }*/
+        return ResponseEntity.notFound().build();
+    }
 
+    @DeleteMapping("/mypage")
+    public ResponseEntity<UsersDto> deleteUser(@AuthenticationPrincipal String userId) {
+        Users user = usersRepository.findById(Integer.parseInt(userId));
 
-//}
+        if (user != null) {
+            String email = user.getEmail();
+            myPageService.deleteUser(userId, email);
+            return ResponseEntity.ok().build();
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+}
